ca0870079ced7c5c5bdff2178b457a8fcd6e66c4
Yue Ren <yue.ren.kl@gmail.com>
2026-08-29 10:57:10 +0100

    fix: surpress compile log popup in emacsclient (Emacs 31.1)

--- early-init.el
+++ early-init.el
@@ -1,5 +1,10 @@
 ;; -*- lexical-binding: t; -*-
 
+;; disable *Compile-Log* popup present after upgrade to Emacs 31.1
+(add-to-list 'display-buffer-alist
+             '("\\*Compile-Log\\*" (display-buffer-no-window)
+               (allow-no-window . t)))
+
 ;; increase garbage collection threshold temporarily to speed up startup
 (defun restore-gc-cons-threshold ()
   (setq gc-cons-threshold (* 16 1024 1024)
